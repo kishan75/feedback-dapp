1bd6aa59c1d30bc4831a0aeae3a265d482b8987c
kishan75 <kishan.chaurasiya.75@gmail.com>
2022-03-20 02:20:28 +0530

    added urls of feedback course

---
+++ client/src/components/feedbacks.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Grid,
+  Box,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Stack,
+  Button,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useParams } from "react-router";
+
+export const FeedbackCard = (props) => {
+  const [expandAccordian, setAccordian] = useState(false);
+  const [accordianHeader, setAccordianHeader] = useState(null);
+
+  useEffect(() => {
+    let splits = props.content.split(" ");
+    let head = "";
+    if (splits.length <= 4) {
+      splits.forEach((word) => (head += word + " "));
+    } else {
+      head = `${splits[0]} ${splits[1]} ${splits[2]} ${splits[3]}`;
+    }
+    head += "...";
+    setAccordianHeader(head);
+  }, []);
+
+  return (
+    <div>
+      {{ accordianHeader } && (
+        <Card
+          onMouseEnter={() => setAccordian(true)}
+          onMouseLeave={() => setAccordian(false)}
+          onClick={() => {
+            window.location.href = `${props.email}/${props.year}/${props.semester}/${props.code}`;
+          }}
+          style={{
+            backgroundColor: "white",
+            boxShadow: "none",
+            border: "4px solid",
+            borderColor: "red",
+            "&:hover": {
+              backgroundColor: "blue !important",
+            },
+          }}
+        >
+          <CardContent>
+            <Accordion expanded={expandAccordian}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                {!expandAccordian && (
+                  <Typography variant="h5" color="text.primary">
+                    {accordianHeader}
+                  </Typography>
+                )}
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography variant="h5" color="text.primary">
+                  {props.content}
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+            {props.skills.map((skill) => {
+              return (
+                <Button
+                  style={{
+                    margin: "5px",
+                  }}
+                  variant="outlined"
+                >
+                  {skill}
+                </Button>
+              );
+            })}{" "}
+          </CardContent>
+        </Card>
+      )}
+    </div>
+  );
+};
+
+export const Feedbacks = (props) => {
+  const [feedbacks, setFeedbacks] = useState(null);
+  const { email, year, sem, courseCode } = useParams();
+
+  useEffect(() => {
+    if (props.courses && email)
+      setFeedbacks(props.courses[email][year][sem][courseCode]);
+  }, [props.courses, email]);
+  return (
+    <div>
+      {feedbacks == null ? (
+        props.showLoader(true)
+      ) : (
+        <Box m={4}>
+          <Grid container spacing={4}>
+            {feedbacks.map((feedback, i) => {
+              return (
+                <Grid item xs={3} key={i.toString()}>
+                  <FeedbackCard {...feedback}></FeedbackCard>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Box>
+      )}
+    </div>
+  );
+};
---
+++ client/src/components/utils/common.js
@@ -0,0 +1,14 @@
+import { styled } from "@mui/material/styles";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import * as React from "react";
+
+export const BootstrapTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} arrow classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.arrow}`]: {
+    color: theme.palette.common.black,
+  },
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.black,
+  },
+}));
