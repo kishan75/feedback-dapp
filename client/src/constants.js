668bff3184f3dbe34ae66bce4b0185ae9b41593a
BinitDOX <yee80andres@gmail.com>
2022-04-10 04:13:57 +0530

    Redeployed chain

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
 export const PYTHON_BASE_URL = "https://feedback-dapp.azurewebsites.net/";
 //export const PYTHON_BASE_URL = "http://127.0.0.1:5000/";
 export const NETWORK_ID = 3;
-export const BHUTOKEN_ADD = "0x6FB250d27F273d31d37f9D2CD292549DECDF50FC";
-export const FEEDBACKDATA_ADD = "0x4b529ee68745f7c4b0e51FFB3fE0cE537e0922A9";
+export const BHUTOKEN_ADD = "0xE578ec6a0661199240Ba1B12441c77fEEaDC3ce6";
+export const FEEDBACKDATA_ADD = "0x33a80C7765964d91fFf2906B03CB93c1Ce9CD87c";
