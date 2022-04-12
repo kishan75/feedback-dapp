ce509874fc6a3115505359e711176745225dabf6
kishan75 <kishan.chaurasiya.75@gmail.com>
2022-04-12 13:09:47 +0530

    final replaced

--- truffle-config.js
+++ truffle-config.js
@@ -60,6 +60,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
+      networkCheckTimeout:10000,
       provider: () =>
         new HDWalletProvider(
           process.env.MNEMONIC,
