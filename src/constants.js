3e11b809de8342c8236cdedec8aec55dc3985b31
kishan75 <kishan.chaurasiya.75@gmail.com>
2022-04-12 13:26:05 +0530

    replaced final

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const PYTHON_BASE_URL = "https://feedback-dapp.azurewebsites.net/";
 //export const PYTHON_BASE_URL = "http://127.0.0.1:5000/";
 export const NETWORK_ID = 3;
-export const BHUTOKEN_ADD = "0x763Ba72e41aC0935880E3Af01dFD51645a19b8D5";
-export const FEEDBACKDATA_ADD = "0xdf3B8cb2CBf4fDB4b5B00264d4134BaE61bcBCC7";
+export const BHUTOKEN_ADD = "0x4e945BF99692B946F96CEca13d16ff3234920215";
+export const FEEDBACKDATA_ADD = "0x60238C27FFA722Fa62FD5A2205A8bDE942303976";
