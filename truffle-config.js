1229b0e0a5158cfa86126bcca8c52f964e62943d
BinitDOX <yee80andres@gmail.com>
2022-11-29 21:45:34 +0530

    Reployed on Goerli

--- truffle-config.js
+++ truffle-config.js
@@ -45,7 +45,7 @@ module.exports = {
     //
     development: {
       host: "127.0.0.1", // Localhost (default: none)
-      port: 7545, // Standard Ethereum port (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
     },
     // Another network with more advanced options...
@@ -59,14 +59,26 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    ropsten: {
+    // ropsten: {
+    //   networkCheckTimeout:10000,
+    //   provider: () =>
+    //     new HDWalletProvider(
+    //       process.env.MNEMONIC,
+    //       `https://ropsten.infura.io/v3/${process.env.PROJECT_ID}`
+    //     ),
+    //   network_id: 3, // Ropsten's id
+    //   gas: 5500000, // Ropsten has a lower block limit than mainnet
+    //   timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+    //   skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    // },
+    goerli: {
       networkCheckTimeout:10000,
       provider: () =>
         new HDWalletProvider(
           process.env.MNEMONIC,
-          `https://ropsten.infura.io/v3/${process.env.PROJECT_ID}`
+          `https://goerli.infura.io/v3/${process.env.PROJECT_ID}`
         ),
-      network_id: 3, // Ropsten's id
+      network_id: 5, // Ropsten's id
       gas: 5500000, // Ropsten has a lower block limit than mainnet
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
